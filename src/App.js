853229ec8092e23e3e5c7d95a2d59fe43d7e47c6
ivan augusto <ivan.augusto@unemat.br>
2021-10-08 10:47:38 -0300

    ci.yml

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React HAHA
+          Learn React
         </a>
       </header>
     </div>
